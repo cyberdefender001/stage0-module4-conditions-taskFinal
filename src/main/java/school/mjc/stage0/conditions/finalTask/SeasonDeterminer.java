b757504c7371d19e358f05122592f035348d2a04
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-09-03 18:39:24 +0500

    Update SeasonDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,25 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+        switch(monhNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Winter");
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Spring");
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Summer");
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Autumn");
+            default:
+                System.out.println("Wrong month number");
+        }
     }
 }
