3bb89eb892d81c508ac48fb2c16032090014d738
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-09-09 14:49:19 +0500

    Update CoordinatePane.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        if (x > 0) {
+            if (y > 0) {
+                System.out.println(1);
+            } else {
+                System.out.println(4);
+            }
+        } else {
+            if (y > 0) {
+                System.out.println(2);
+            } else {
+                System.out.println(3);
+            }
+        }
     }
 }
