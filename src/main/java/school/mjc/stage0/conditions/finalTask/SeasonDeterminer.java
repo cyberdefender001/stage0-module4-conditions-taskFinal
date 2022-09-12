ffcf801cdc4c2f7fdca0a9d675d3ace7280954a4
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-09-12 16:16:20 +0500

    Update SeasonDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
-        switch(monhNumber) {
+        switch(monthNumber) {
             case 1:
             case 2:
             case 12:
